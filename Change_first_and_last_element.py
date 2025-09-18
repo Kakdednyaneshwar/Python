c1b5671004f3355fc2e192a0c068f7d067ad2d39
Kakde Dnyaneshwar <118787516+Kakdednyaneshwar@users.noreply.github.com>
2025-09-18 22:37:06 +0530

    Add files via upload

---
+++ Change_first_and_last_element.py
@@ -0,0 +1,10 @@
+#Python_program_to_interchange_first_and_last_elements_in_a_list
+def swap(list):
+    a=list[0],list[-1]=list[-1],list[0]
+    
+    return list 
+    
+# main code or driver code
+list = [10,20,30,40,50]
+print("before changing first and last element :",list)
+print("After changing first and last element :",swap(list))
