1218cb95f361799d130509470561329d4551b471
Kakde Dnyaneshwar <118787516+Kakdednyaneshwar@users.noreply.github.com>
2025-09-18 22:52:19 +0530

    Add files via upload

---
+++ Greater_num.py
@@ -0,0 +1,10 @@
+#Program to find greater of 2 entered number
+x = int(input("Enter a Number :"))
+y = int(input("Enter a Number :"))
+
+if x > y :
+    print("Number is Greater :",x)
+elif x == y:
+    print("Both Number is Same")
+else:
+    print("Number is Greater :",y)
