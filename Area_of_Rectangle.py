8d38221ab7951ccb55fb60d90fd13928542bf182
Kakde Dnyaneshwar <118787516+Kakdednyaneshwar@users.noreply.github.com>
2025-09-18 22:41:06 +0530

    Add files via upload

---
+++ Area_of_Rectangle.py
@@ -0,0 +1,11 @@
+################################################################
+#Write a program to find area of rectangle.
+
+length = float(input("Enter the length of rectangle :"))
+width = float(input("Enter the width of rectangle :"))
+
+area = length * width
+
+print("The Area of rectangle is :",area)
+
+####################################################################
